4db28381d9019708d5814185124e8b2909e387be
mateuszliber <mateusz.liber@smcebi.edu.pl>
2020-02-07 13:05:47 +0100

    Layout changes

--- app/src/main/java/com/liber/organizer/TaskActivity.kt
+++ app/src/main/java/com/liber/organizer/TaskActivity.kt
@@ -47,13 +47,13 @@ class TaskActivity : AppCompatActivity() {
         taskGradeRatingBar = findViewById(R.id.taskGradeRatingBar)
 
         buttonGoals.setOnClickListener {
-            var intent = Intent(context, TaskGoalsActivity::class.java)
+            val intent = Intent(context, TaskGoalsActivity::class.java)
             intent.putExtra("task", taskItem)
             startActivity(intent)
         }
 
         btnEdit.setOnClickListener {
-            var intent = Intent(context, EditTaskActivity::class.java)
+            val intent = Intent(context, EditTaskActivity::class.java)
             intent.putExtra("task", taskItem)
             startActivity(intent)
         }
@@ -64,16 +64,17 @@ class TaskActivity : AppCompatActivity() {
             builder.setTitle("Deleting Task")
             builder.setMessage("WARNING: Are you sure you want to delete this Task? All of its data like grades and statistics will be also deleted!")
 
-            builder.setPositiveButton("Confirm Delete"){dialog, which ->
-                if(db.deleteTask(taskItem.taskId)) {
+            builder.setPositiveButton("Confirm Delete") { dialog, which ->
+                if (db.deleteTask(taskItem.taskId)) {
                     db.deleteAllTaskGrades(taskItem.taskId)
-                    Toast.makeText(applicationContext,"Successfully deleted.",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "Successfully deleted.", Toast.LENGTH_SHORT)
+                        .show()
                 }
                 finish()
             }
 
-            builder.setNegativeButton("Cancel"){dialog,which ->
-                Toast.makeText(applicationContext,"Nothing deleted",Toast.LENGTH_SHORT).show()
+            builder.setNegativeButton("Cancel") { dialog, which ->
+                Toast.makeText(applicationContext, "Nothing deleted", Toast.LENGTH_SHORT).show()
             }
 
             val dialog: AlertDialog = builder.create()
@@ -94,7 +95,7 @@ class TaskActivity : AppCompatActivity() {
         taskDescriptionTextView.text = taskItem.taskDescription
         taskGradeTextView.text = taskItem.taskAvarage.toString()
 
-        if(taskItem.taskAvarage != 0.toDouble()) {
+        if (taskItem.taskAvarage != 0.toDouble()) {
             taskGradeRatingBar.rating = taskItem.taskAvarage.toFloat()
 
         } else {
@@ -104,61 +105,69 @@ class TaskActivity : AppCompatActivity() {
         taskEvaluationTime.text = getTime(taskItem.taskEvaluationTime)
 
 
-        setUpPieChartData(taskItem.taskId)
+        setUpLineChartData(taskItem.taskId)
     }
 
-    fun getDay(dayId: Int) : String {
-        if(dayId < OUT_OF_WEEK) {
+    fun getDay(dayId: Int): String {
+        if (dayId < OUT_OF_WEEK) {
             return resources.getStringArray(R.array.daysOfWeek)[dayId]
         } else return EVALUATE_EVERYDAY_STRING
     }
 
-    fun getTime(timeInMillis: Long) : String {
+    fun getTime(timeInMillis: Long): String {
         val hours = timeInMillis / 1000L / 60 / 60
         val minutes = timeInMillis / 1000L / 60 % 60
 
-        if(minutes == 0L) {
+        if (minutes == 0L) {
             return "${hours}:00"
         }
 
         return "${hours}:${minutes}"
     }
 
-    private fun setUpPieChartData(taskId: Int) {
+    private fun setUpLineChartData(taskId: Int) {
+        //Dataset
         val yVals = ArrayList<Entry>()
-        val taskGrades = db.readGrades(taskId)
+        val taskGrades = db.readGrades(taskId).reversed()
 
-        for(i in 0..(taskGrades.size - 1)) {
-            yVals.add(Entry(taskGrades[i].gradeGrade.toFloat(), i.toFloat() , i.toString()))
+        for (i in 0..(taskGrades.size - 1)) {
+            if(taskGrades[i].gradeGrade != 0) {
+                yVals.add(Entry(i.toFloat(), taskGrades[i].gradeGrade.toFloat(), i.toString()))
+            }
         }
 
-        yVals.add(Entry(11f, 28f, "11"))
-
         val set1: LineDataSet
-        set1 = LineDataSet(yVals, "DataSet 1")
+        set1 = LineDataSet(yVals, "All ${db.readTask(taskId).taskName} grades")
 
+        //Chart colors
         set1.color = Color.BLUE
         set1.setCircleColor(Color.BLUE)
+
+        //Line
         set1.lineWidth = 1f
-        set1.circleRadius = 3f
+
+        //Points settings
+        set1.circleRadius = 5f
         set1.setDrawCircleHole(false)
-        set1.valueTextSize = 0f
+        set1.valueTextSize = 1f
         set1.setDrawFilled(false)
 
         val dataSets = ArrayList<ILineDataSet>()
         dataSets.add(set1)
         val data = LineData(dataSets)
 
-        // set data
+        //Chart options
         taskGradesLineChart.setData(data)
         taskGradesLineChart.description.isEnabled = false
-        taskGradesLineChart.legend.isEnabled = false
+//        taskGradesLineChart.legend.isEnabled = false
         taskGradesLineChart.setPinchZoom(true)
-        taskGradesLineChart.xAxis.enableGridDashedLine(5f, 5f, 0f)
-        taskGradesLineChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
-        taskGradesLineChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
-        //lineChart.setDrawGridBackground()
-        taskGradesLineChart.xAxis.labelCount = 11
+        taskGradesLineChart.legend.textSize = 15f
+        taskGradesLineChart.legend.formSize = 15f
+//        taskGradesLineChart.xAxis.enableGridDashedLine(5f, 5f, 0f)
+//        taskGradesLineChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
+//        taskGradesLineChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
+        taskGradesLineChart.setDrawGridBackground(false)
+        taskGradesLineChart.xAxis.labelCount = 14
         taskGradesLineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
     }
 }
